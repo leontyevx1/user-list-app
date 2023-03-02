e6c717ba14b510d1cc9029528bacd72763579d72
Anton Leontyev <leontyevx2@gmail.com>
2023-03-02 23:22:51 +0700

    Create user list app with search user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './index.scss';
+import {Success} from './components/Success';
+import {Users} from './components/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [users, setUsers] = useState([]);
+    const [invites, setInvites] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [success, setSuccess] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+
+    React.useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((response) => response.json())
+            .then((json) => {
+                setUsers(json.data);
+            }).catch((error) => {
+            console.warn(error);
+            alert('Ошибка при запросе списка пользователей!')
+        }).finally(() => {
+            setLoading(false);
+        })
+
+    }, [])
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value);
+    }
+
+    const onClickInvite = (id) => {
+        if (invites.includes(id)) {
+            setInvites((prev) => prev.filter((_id) => _id !== id));
+        } else {
+            setInvites((prev) => [...prev, id]);
+        }
+    }
+
+    const onClickSentInvites = () => {
+        setSuccess(true);
+    }
+
+    return (
+        <div className="App">
+            {success ?
+                (<Success count={invites.length}/>
+                ) : (
+                    <Users searchValue={searchValue}
+                           onChangeSearchValue={onChangeSearchValue}
+                           items={users}
+                           isLoading={isLoading}
+                           onClickInvite={onClickInvite}
+                           invites={invites}
+                           onClickSentInvites={onClickSentInvites}/>
+                )}
+        </div>
+    );
 }
 
 export default App;
